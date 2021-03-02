f5fc83c740da163205e957f8e51252b0584dabb7
vagrant <vagrant@homestead>
2021-03-02 03:35:05 +0000

    first commit

---
+++ src/IdCard/Common/AbstractClient.php
@@ -0,0 +1,11 @@
+<?php
+namespace IdCard\Common;
+
+/**
+ * 抽象api类，禁止client引用
+ * @package IdCard\Common
+ */
+abstract class AbstractClient
+{
+	
+}
---
+++ src/IdCard/FCM/V20210302/FCMClient.php
@@ -0,0 +1,10 @@
+<?php
+namespace IdCard\FCM\V20210302;
+
+class FCMClient extends AbstractClient
+{
+	public function index()
+	{
+		 echo "this is FCMClient";
+	}
+}
